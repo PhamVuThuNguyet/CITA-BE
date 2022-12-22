8ab80f3713f0d0a7a1962b94a56a839cf21bcf0a
htphong01 <thanhphonga4.hbt@gmail.com>
2022-12-22 21:58:50 +0700

    change database link

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
-const DB_CONNECTION_ON_CLOUD = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cita.vxfb4tx.mongodb.net/cita?retryWrites=true&w=majority`
+const DB_CONNECTION_ON_CLOUD = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@databases.n6njyvb.mongodb.net/cita?retryWrites=true&w=majority`
 
 module.exports = {
   DB_CONNECTION_ON_CLOUD,

f62c3091936ed1210e2a36352b3407fbf8dbebee
PhamVuThuNguyet <nguyet.pham.researcher@gmail.com>
2024-06-13 05:42:28 +0000

    update be

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 require('dotenv').config()
 
-const DB_CONNECTION_ON_CLOUD = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@databases.n6njyvb.mongodb.net/cita?retryWrites=true&w=majority`
+// const DB_CONNECTION_ON_CLOUD = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cita.vxfb4tx.mongodb.net/cita?retryWrites=true&w=majority`;
+const DB_CONNECTION = "mongodb://localhost:27017/cita";
 
 module.exports = {
-  DB_CONNECTION_ON_CLOUD,
+  DB_CONNECTION_ON_CLOUD: DB_CONNECTION,
 }
